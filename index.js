fdb78b6ca217f7314e539ebeedc8df23a3d182f2
dishant <dishu5570@gmail.com>
2024-02-06 09:47:51 +0530

    ask

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const { askToGPT } = require('./controllers/index.js');
+const cors = require('cors');
 
 dotenv.config();
 
 const app = express();
 
 app.use(express.json());
+
+//cors all origins access
+app.use(cors(
+  {
+    origin: '*'
+  }
+));
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
